f5af9034a7e3e89463a07343c80a0b1271ad6f24
Phạm Quang Ngà <okamibada@gmail.com>
2026-01-30 14:57:39 +0700

    add partner-section in homepage

--- front-page.php
+++ front-page.php
@@ -8,5 +8,6 @@ get_template_part('template-parts/homepage/featured-section');
 get_template_part('template-parts/homepage/map-section');
 get_template_part('template-parts/homepage/benefit-section');
 get_template_part('template-parts/homepage/brand-section');
+get_template_part('template-parts/homepage/partners-section');
 get_footer();
 ?>
