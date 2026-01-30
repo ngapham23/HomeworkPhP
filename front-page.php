8d1ac7fb45a2d99497ad24be28e6b19e8bb77681
Phạm Quang Ngà <okamibada@gmail.com>
2026-01-30 09:27:23 +0700

    add    map-section in homepage

--- front-page.php
+++ front-page.php
@@ -5,5 +5,6 @@ Template Name: Home Page
 get_header();
 get_template_part('template-parts/homepage/hero-section');
 get_template_part('template-parts/homepage/featured-section');
+get_template_part('template-parts/homepage/map-section');
 get_footer();
 ?>
