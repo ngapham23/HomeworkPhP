ff4fc76f82651517fd6dc1ad85b7c692b5323ac1
Phạm Quang Ngà <okamibada@gmail.com>
2026-02-04 07:48:26 +0700

    update content 404 page

--- 404.php
+++ 404.php
@@ -3,8 +3,18 @@ if (!defined('ABSPATH'))
     exit;
 get_header();
 ?>
-<div class="aps-404">
-    <img src="<?php echo esc_url(aps_img . '404/404.jpg'); ?>" alt="404 Not Found" class="aps-404__img" />
-    <a href="<?php echo esc_url(home_url('/')); ?>" class="aps-404__btn">Go Home</a>
-</div>
-<?php get_footer(); ?>
+
+<section class="aps-error-page">
+    <div class="aps-container">
+        <div class="aps-error-content">
+            <div class="aps-error-img">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/image/404/404.jpg'); ?>" alt="404 Not Found" />
+            </div>
+            <h1 class="aps-error-title">Page Not Found</h1>
+            <p class="aps-error-desc">We're sorry, the page you requested could not be found. <br> Please go back to the homepage.</p>
+            <a href="<?php echo esc_url(home_url('/')); ?>" class="aps-btn--error">Back to Home</a>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>  
