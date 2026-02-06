c5ca66bcec5b6f9bb4219a3512bbd154ae9c976b
Phạm Quang Ngà <okamibada@gmail.com>
2026-02-06 15:34:51 +0700

    modify post equipment no limited

--- template-parts/singlepage/locationsingle/equipment-section.php
+++ template-parts/singlepage/locationsingle/equipment-section.php
@@ -5,6 +5,33 @@ if ($equipments && is_array($equipments)): ?>
         <h4 class="equipment-list--title">Equipment List</h4>
         <div class="location-equipment-listing">
             <?php
+            foreach ($equipments as $p):
+                $post_id = is_object($p) ? $p->ID : $p;
+                $thumb = get_the_post_thumbnail_url($post_id, 'medium');
+                if (!$thumb) {
+                    $thumb = get_template_directory_uri() . '/assets/image/logo.png';
+                }
+                $post_object = get_post($post_id);
+                if ($post_object) {
+                    setup_postdata($GLOBALS['post'] = &$post_object);
+                    get_template_part('template-parts/components/equipment-card', null, array('thumbnail' => $thumb));
+                }
+            endforeach;
+            wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+<?php endif; ?>
+
+
+<!-- limitid 11 post -->
+<!-- <?php
+        $equipments = get_field('featured_equipment');
+        if ($equipments && is_array($equipments)): ?>
+    <div class="equipment-list-section">
+        <h4 class="equipment-list--title">Equipment List</h4>
+        <div class="location-equipment-listing">
+            <?php
             $equipments_display = array_slice($equipments, 0, 11);
             foreach ($equipments_display as $p):
                 $post_id = is_object($p) ? $p->ID : $p;
@@ -22,4 +49,4 @@ if ($equipments && is_array($equipments)): ?>
             ?>
         </div>
     </div>
-<?php endif; ?>
+<?php endif; ?> -->
